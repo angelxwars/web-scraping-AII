3b28d74b78478d25d7b6b7e78088450ae7a8505a
Daniel <dhidalgosanchez96@gmail.com>
2017-12-23 17:43:59 +0100

    Add get recipe links in a recipe book

--- index.py
+++ index.py
@@ -2,6 +2,8 @@ from urllib import request
 from bs4 import BeautifulSoup
 
 
+# open any url
+
 def open_url(url):
     try:
         page = request.urlopen(url)
@@ -14,19 +16,36 @@ def open_url(url):
 # Get list with recipe book title and recipe book link
 
 def get_recipe_books(url):
-    threads = []
+    books = []
     page = open_url(url)
     soup = BeautifulSoup(page, 'html.parser')
-
     div_recipe_books = soup.find_all(attrs={'class': 'col-md-3'})
 
-    for recipe_book in div_recipe_books:
-        recipe_link = recipe_book.find('div').find('a').get('href')
-        recipe_title = recipe_book.find('div').find('a').find(attrs={'class': 'titleLista'}).find('h2').text
+    for book in div_recipe_books:
+        recipe_link = book.find('div').find('a').get('href')
+        recipe_title = book.find('div').find('a').find(attrs={'class': 'titleLista'}).find('h2').text
+        books.append({'recipe_book_title': recipe_title, 'recipe_book_link': recipe_link})
 
-        threads.append({'recipe_title': recipe_title, 'recipe_link': recipe_link})
-
-    return threads
+    return books
 
 
-print(get_recipe_books('http://www.recetags.com/recetarios'))
+# Get recipe links in a recipe_book
+
+def get_recipes_link_by_book(book):
+    recipes_link = []
+    page = open_url(book['recipe_book_link'])
+    soup = BeautifulSoup(page, 'html.parser')
+    div_recipes = soup.find_all(attrs={'class': 'cajaReceta'})
+
+    for recipe in div_recipes:
+        link = recipe.find(attrs={'class': 'staticrcp'}).find('a').get('href')
+        recipes_link.append(link)
+
+    return recipes_link
+
+
+
+recipe_books = get_recipe_books('http://www.recetags.com/recetarios')
+
+for recipe_book in recipe_books:
+    print(get_recipes_link_by_book(recipe_book))
